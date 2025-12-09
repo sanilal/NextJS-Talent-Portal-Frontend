efa9c2cff6e8ffa6fef8f4314e548cad7466c44c
Sanilal <sanilal@gmail.com>
2025-12-09 11:36:07 +0400

    alert tsx syntax issue

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -17,7 +17,7 @@ const alertVariants = cva(
   }
 )
 
-const Alert = React.forwardRef
+const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
 >(({ className, variant, ...props }, ref) => (
@@ -30,7 +30,7 @@ const Alert = React.forwardRef
 ))
 Alert.displayName = "Alert"
 
-const AlertTitle = React.forwardRef
+const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
@@ -42,7 +42,7 @@ const AlertTitle = React.forwardRef
 ))
 AlertTitle.displayName = "AlertTitle"
 
-const AlertDescription = React.forwardRef
+const AlertDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
